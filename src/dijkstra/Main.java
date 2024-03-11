6c451ce695a2de1a1d9492a5b69072657924d47c
ManojKannanD <fabulousmanoj27@gmail.com>
2024-03-11 19:14:47 +0530

    dijkstra implementation

---
+++ src/dijkstra/Main.java
@@ -0,0 +1,32 @@
+package dijkstra;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Node<String> nodeA = new Node<>("A");
+        Node<String> nodeB = new Node<>("B");
+        Node<String> nodeC = new Node<>("C");
+        Node<String> nodeD = new Node<>("D");
+        Node<String> nodeE = new Node<>("E");
+        Node<String> nodeF = new Node<>("F");
+
+        nodeA.addAdjacentNode(nodeB, 2);
+        nodeA.addAdjacentNode(nodeC, 4);
+
+        nodeB.addAdjacentNode(nodeC, 3);
+        nodeB.addAdjacentNode(nodeD, 1);
+        nodeB.addAdjacentNode(nodeE, 5);
+
+        nodeC.addAdjacentNode(nodeD, 2);
+
+        nodeD.addAdjacentNode(nodeE, 1);
+        nodeD.addAdjacentNode(nodeF, 4);
+
+        nodeE.addAdjacentNode(nodeF, 2);
+
+        Dijkstra<String> dijkstra = new Dijkstra<>();
+        dijkstra.calculateShortestPath(nodeA);
+        dijkstra.printPaths(Arrays.asList(nodeA, nodeB, nodeC, nodeD, nodeE, nodeF));
+    }
+}
