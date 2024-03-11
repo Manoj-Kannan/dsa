cbab775185c3590ecaadc43fcf95aeb193eca0cd
ManojKannanD <fabulousmanoj27@gmail.com>
2024-03-11 19:00:04 +0530

    dfs implementation

---
+++ src/dfs/DepthFirstSearch.java
@@ -0,0 +1,35 @@
+package dfs;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DepthFirstSearch<T> {
+
+    // This one will visit the last child first
+    public void traverse(Vertex<T> startVertex) {
+        Deque<Vertex<T>> stack = new LinkedList<>();
+        stack.push(startVertex);
+
+        while (!stack.isEmpty()) {
+            Vertex<T> current = stack.pop();
+            if (!current.isVisited()) {
+                current.setVisited(true);
+                System.out.println(current);
+                // to preserve order of child without recursive approach (we can reverse the neighbours before push to stack
+                // (since stack follows LIFO)
+                current.getNeighbors().forEach(stack::push);
+            }
+        }
+    }
+
+    // This one will preserve the order of the children
+    public void traverseRecursively(Vertex<T> vertex) {
+        vertex.setVisited(true);
+        System.out.println(vertex);
+        for (Vertex<T> neighbor : vertex.getNeighbors()) {
+            if (!neighbor.isVisited()) {
+                traverseRecursively(neighbor);
+            }
+        }
+    }
+}
---
+++ src/dfs/Main.java
@@ -0,0 +1,30 @@
+package dfs;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Order of Child is not maintained
+        new DepthFirstSearch<Integer>().traverse(getSoueceVertex());
+        System.out.println();
+        // Order of Child is maintained
+        new DepthFirstSearch<Integer>().traverseRecursively(getSoueceVertex());
+    }
+
+    private static Vertex<Integer> getSoueceVertex() {
+        Vertex<Integer> v0 = new Vertex<>(0);
+        Vertex<Integer> v1 = new Vertex<>(1);
+        Vertex<Integer> v2 = new Vertex<>(2);
+        Vertex<Integer> v3 = new Vertex<>(3);
+        Vertex<Integer> v4 = new Vertex<>(4);
+        Vertex<Integer> v5 = new Vertex<>(5);
+        Vertex<Integer> v6 = new Vertex<>(6);
+
+        v0.setNeighbors(Arrays.asList(v1, v5, v6));
+        v1.setNeighbors(Arrays.asList(v3, v4, v5));
+        v4.setNeighbors(Arrays.asList(v2, v6));
+        v6.setNeighbors(Arrays.asList(v0));
+
+        return v0;
+    }
+}
