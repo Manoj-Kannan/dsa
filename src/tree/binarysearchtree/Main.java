4151d6a5714a56778311b21297ff2e6babca4e3a
ManojKannanD <fabulousmanoj27@gmail.com>
2024-03-13 11:01:23 +0530

    bst implementation

---
+++ src/tree/binarysearchtree/Main.java
@@ -0,0 +1,21 @@
+package tree.binarysearchtree;
+
+import tree.Tree;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Tree<Integer> bst = new BinarySearchTree<>();
+        bst.insert(10).insert(20).insert(30).insert(40).insert(50).insert(60);
+        bst.traverse();
+
+        System.out.println("Max is: " + bst.getMax());
+        System.out.println("Min is: " + bst.getMin());
+
+        System.out.println("Deleting 60 from Tree");
+        bst.delete(60);
+        System.out.println("New Max is: " + bst.getMax());
+        bst.traverse();
+
+    }
+}
