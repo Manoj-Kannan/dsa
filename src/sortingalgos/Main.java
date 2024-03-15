a032dd787376e7f1a8e60082a231951066ee7a01
ManojKannanD <fabulousmanoj27@gmail.com>
2024-03-15 15:22:57 +0530

    SelectionSort  & InsertionSort impl

--- src/sortingalgos/Main.java
+++ src/sortingalgos/Main.java
@@ -8,8 +8,18 @@ public class Main {
         String [] strings = { "Sylvanas", "Arthas", "Illidan", "Thrall", "Jaina" };
         System.out.println(Arrays.toString(integers) + " | " + Arrays.toString(strings));
 
-        new BubbleSort<>(integers).sort();
-        new BubbleSort<>(strings).sort();
-        System.out.println(Arrays.toString(integers) + " | " + Arrays.toString(strings));
+        sortAndPrint(new BubbleSort<>(integers.clone()));
+        sortAndPrint(new BubbleSort<>(strings.clone()));
+
+        sortAndPrint(new SelectionSort<>(integers.clone()));
+        sortAndPrint(new SelectionSort<>(strings.clone()));
+
+        sortAndPrint(new InsertionSort<>(integers.clone()));
+        sortAndPrint(new InsertionSort<>(strings.clone()));
+    }
+
+    private static <T extends Comparable<T>> void sortAndPrint(Sort<T> sortingAlgorithm) {
+        sortingAlgorithm.sort();
+        System.out.println(Arrays.toString(sortingAlgorithm.arr));
     }
 }
